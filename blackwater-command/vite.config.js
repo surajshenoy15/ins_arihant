fd8f8e10ece12a70196560686ef0a74c65339774
suraj shenoy <surajshenoyp@gmail.com>
2026-03-24 09:59:38 +0530

    udpated

--- blackwater-command/vite.config.js
+++ blackwater-command/vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    basicSsl(),
-  ],
+  plugins: [react()],
   server: {
-    https: true,
     host: true,
     port: 5173,
   },
